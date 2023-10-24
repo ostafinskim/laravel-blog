324fed57b292035018949a0205c47affb3243030
Miro Ostafiński <ostafinskim@gmail.com>
2023-10-24 17:05:48 +0100

    postController and search

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 use App\Models\Post;
 use App\Models\User;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,18 +17,9 @@ use App\Models\User;
 |
 */
 
-Route::get('/', function () {
-     return view('posts', [
-         'posts' => Post::latest()->with('category', 'author')->get(),
-         'categories' => Category::all()
-     ]);
-})->name('home');
+Route::get('/', [PostController::class, 'index'])->name('home');
 
-Route::get('posts/{post:slug}', function (Post $post) {
-    return view('post', [
-        'post' => $post
-    ]);
-});
+Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
 Route::get('categories/{category:name}', function (Category $category) {
     return view('posts', [
