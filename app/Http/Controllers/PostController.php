050e6084407d2c839317a52e53a11d7186ba78cd
Miro Ostafiński <ostafinskim@gmail.com>
2023-10-27 14:47:35 +0100

    query bug

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,8 +10,7 @@ class PostController extends Controller
 {
     public function index() {
          return view('posts.index', [
-             'posts' => Post::latest()->filter(request(['search', 'category']))->get(),
-             'categories' => Category::all(),
+             'posts' => Post::latest()->filter(request(['search', 'category', 'author']))->get(),
          ]);
     }
 
--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,8 @@
 <?php
 
-use App\Http\Controllers\AuthorController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
-
-Route::get('authors/{author:username}', [AuthorController::class, 'index']);
